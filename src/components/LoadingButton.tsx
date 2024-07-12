f0b386b146b4394a735a8f647b9eb952237c9ab1
Harshalvk <harshalvkhobragade@gmail.com>
2024-07-13 00:07:51 +0530

    ADD: Hook to frontend to fetch order details on Payments page

--- src/components/LoadingButton.tsx
+++ src/components/LoadingButton.tsx
@@ -2,7 +2,7 @@ import { Button } from "./ui/button";
 import { Loader2 } from "lucide-react";
 const LoadingButton = () => {
   return (
-    <Button disabled>
+    <Button disabled className="w-full">
       <Loader2 className="mr-2 h-4 w-4 animate-spin" />
       Loading
     </Button>
